9065b9cc415f17a1b0d7d327844fca4c952c5be0
Thiago dos Santos Barbosa <thiagocomsilva10@gmail.com>
2025-04-24 17:56:34 -0300

    refatora a classe Produto para melhorar a formatação e adiciona nova tabela de produto no banco de dados

--- src/main/java/com/CRM/HKCRM2/model/Produto.java
+++ src/main/java/com/CRM/HKCRM2/model/Produto.java
@@ -9,9 +9,9 @@ import jakarta.persistence.Entity;
 public class Produto {
 
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
         private String nome;
         private Double preco;
