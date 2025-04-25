5be271938348d538b9c42b817986abaa0cf2ac30
Thiago dos Santos Barbosa <thiagocomsilva10@gmail.com>
2025-04-25 17:47:52 -0300

    ajusta a formatação e a indentação da classe Doce para melhorar a legibilidade do código

--- src/main/java/com/CRM/HKCRM2/model/Doce.java
+++ src/main/java/com/CRM/HKCRM2/model/Doce.java
@@ -1,16 +1,15 @@
 package com.CRM.HKCRM2.model;
 
 import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
-@Entity                             // Define a classe como uma entidade JPA 
-@Table(name = "doce")               // Define o nome da tabela no banco de dados
-@PrimaryKeyJoinColumn(name = "id") // Define a chave primária da tabela 
-public class Doce extends Produto { // A chave primária da tabela Doce é a mesma da tabela Produto. Isso significa que a tabela Doce herda a chave primária da tabela Produto
+@Entity                                             // Define a classe como uma entidade JPA 
+@Table(name = "doce")                               // Define o nome da tabela no banco de dados
+@PrimaryKeyJoinColumn(name = "id")                  // Define a chave primária da tabela 
+public class Doce extends Produto {                 // A chave primária da tabela Doce é a mesma da tabela Produto. Isso significa que a tabela Doce herda a chave primária da tabela Produto
 
     private Integer quantidade;       
     private String sabor;
@@ -19,15 +18,15 @@ public class Doce extends Produto { // A chave primária da tabela Doce é a mes
     private LocalDate dataCompra;           
 
     public Doce() {
-        super();                    // Chama o construtor da classe pai (Produto)
+        super();                                // Chama o construtor da classe pai (Produto)
     }
 
     public Doce(Integer id, String nome, Double preco, String sabor, Integer quantidade, LocalDate dataCompra) {
         // Construtor da classe Doce que recebe parâmetros para inicializar os atributos
-        super(id, nome, preco);      // Chama o construtor da classe pai (Produto)
-        this.sabor = sabor;          // Inicializa o atributo sabor
-        this.quantidade = quantidade; // Inicializa o atributo quantidade
-        this.dataCompra = dataCompra; // Inicializa o atributo dataCompra
+        super(id, nome, preco);                     // Chama o construtor da classe pai (Produto)
+        this.sabor = sabor;                         // Inicializa o atributo sabor
+        this.quantidade = quantidade;               // Inicializa o atributo quantidade
+        this.dataCompra = dataCompra;               // Inicializa o atributo dataCompra
     }
 
     public String getSabor() {
