23cba6ff44e4e5c4a8f22d5fdef522a5d3d72802
Thiago dos Santos Barbosa <thiagocomsilva10@gmail.com>
2025-04-25 17:44:50 -0300

    adiciona atributos quantidade e dataCompra à classe Doce e atualiza a tabela doce no banco de dados; cria a classe Plano como entidade que herda de Produto

--- src/main/java/com/CRM/HKCRM2/model/Doce.java
+++ src/main/java/com/CRM/HKCRM2/model/Doce.java
@@ -1,37 +1,56 @@
 package com.CRM.HKCRM2.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
-@Entity // Define a classe como uma entidade JPA 
-@Table(name = "doce") // Define o nome da tabela no banco de dados
-public class Doce extends Produto { // A classe Doce herda da classe Produto
-    
-    private String categoria; // Atributo específico da classe Doce
-    private String sabor; // Atributo específico da classe Doce
+@Entity                             // Define a classe como uma entidade JPA 
+@Table(name = "doce")               // Define o nome da tabela no banco de dados
+@PrimaryKeyJoinColumn(name = "id") // Define a chave primária da tabela 
+public class Doce extends Produto { // A chave primária da tabela Doce é a mesma da tabela Produto. Isso significa que a tabela Doce herda a chave primária da tabela Produto
+
+    private Integer quantidade;       
+    private String sabor;
+
+    @Column(name = "data_compra", columnDefinition = "DATE NOT NULL DEFAULT CURRENT_DATE")
+    private LocalDate dataCompra;           
 
     public Doce() {
-        super(); // Chama o construtor da classe pai (Produto)
+        super();                    // Chama o construtor da classe pai (Produto)
     }
 
-    public Doce(Integer id, String nome, Double preco, String sabor) {
-        super(id, nome, preco); // Chama o construtor da classe pai (Produto)
-        this.sabor = sabor; // Inicializa o atributo sabor
+    public Doce(Integer id, String nome, Double preco, String sabor, Integer quantidade, LocalDate dataCompra) {
+        // Construtor da classe Doce que recebe parâmetros para inicializar os atributos
+        super(id, nome, preco);      // Chama o construtor da classe pai (Produto)
+        this.sabor = sabor;          // Inicializa o atributo sabor
+        this.quantidade = quantidade; // Inicializa o atributo quantidade
+        this.dataCompra = dataCompra; // Inicializa o atributo dataCompra
     }
 
     public String getSabor() {
-        return sabor; // Retorna o sabor do doce
+        return sabor;               
     }
 
     public void setSabor(String sabor) {
-        this.sabor = sabor; // Define o sabor do doce
+        this.sabor = sabor;         
     }
-    public String getCategoria() {
-        return categoria;
+    public Integer getQuantidade() {
+        return quantidade;
     }
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(LocalDate dataCompra) {
+        this.dataCompra = dataCompra;
     }
 
 }
---
+++ src/main/java/com/CRM/HKCRM2/model/Plano.java
@@ -0,0 +1,11 @@
+package com.CRM.HKCRM2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "plano") // Define o nome da tabela no banco de dados
+public class Plano extends Produto {
+    
+
+}
