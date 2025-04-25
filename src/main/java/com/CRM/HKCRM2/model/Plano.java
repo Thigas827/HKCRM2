f608132c1ba47a3784bbf91221703dd08f6047ef
Thiago dos Santos Barbosa <thiagocomsilva10@gmail.com>
2025-04-25 18:02:51 -0300

    corrige o tipo do atributo tipo_plano na tabela plano de INT para VARCHAR(255) no script SQL

--- src/main/java/com/CRM/HKCRM2/model/Plano.java
+++ src/main/java/com/CRM/HKCRM2/model/Plano.java
@@ -1,11 +1,48 @@
 package com.CRM.HKCRM2.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "plano") // Define o nome da tabela no banco de dados
+@Table(name = "plano")
+@PrimaryKeyJoinColumn(name = "id")                  // Define a chave primária da tabela
 public class Plano extends Produto {
+
+    @Column(name = "tipo_plano")
+    private String tipoPlano;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private LocalDate dataVencimento;
+    private Double valorMensalidade;
+    
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public String getTipoPlano() {
+        return tipoPlano;
+    }
+
+    public void setTipoPlano(String tipoPlano) {
+        this.tipoPlano = tipoPlano;
+    }
     
 
 }
