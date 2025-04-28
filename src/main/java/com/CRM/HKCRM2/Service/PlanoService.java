cb1e1dcb3e7719a7610c6f376ed0384057dddf06
Thiago dos Santos Barbosa <thiagocomsilva10@gmail.com>
2025-04-28 04:09:09 -0300

    remove classes DTOs DoceDtos e PlanoDtos; reimplementa as classes com novas definições

--- src/main/java/com/CRM/HKCRM2/Service/PlanoService.java
+++ src/main/java/com/CRM/HKCRM2/Service/PlanoService.java
@@ -2,10 +2,10 @@ package com.CRM.HKCRM2.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.CRM.HKCRM2.repositories.PlanoRepository;
 import com.CRM.HKCRM2.model.Plano;
-import com.CRM.HKCRM2.model.PlanoDtos;
-import com.CRM.HKCRM2.model.PlanoRepository;
+import com.CRM.HKCRM2.dtos.PlanoDtos;
 
 import java.util.List;
 import java.util.Optional;
