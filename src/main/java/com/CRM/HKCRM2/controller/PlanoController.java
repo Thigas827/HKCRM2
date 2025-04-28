3f35d3237b6c142d5c93a3ff000772a9eaf9c1ae
Thiago dos Santos Barbosa <thiagocomsilva10@gmail.com>
2025-04-28 17:29:05 -0300

    remove import desnecessário de RequestMethod no PlanoController

--- src/main/java/com/CRM/HKCRM2/controller/PlanoController.java
+++ src/main/java/com/CRM/HKCRM2/controller/PlanoController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.bind.annotation.RequestMethod;
 import com.CRM.HKCRM2.Service.PlanoService;
 import com.CRM.HKCRM2.dtos.PlanoDtos;
 import com.CRM.HKCRM2.model.Plano;
