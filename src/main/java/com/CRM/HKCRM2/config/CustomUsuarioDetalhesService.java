e580252cba382ae1455bc143e96cfd386f1ddec2
Thiago dos Santos Barbosa <thiagocomsilva10@gmail.com>
2025-05-05 18:34:26 -0300

    implementa o serviço CustomUsuarioDetalhesService para carregar detalhes do usuário com base no email

--- src/main/java/com/CRM/HKCRM2/config/CustomUsuarioDetalhesService.java
+++ src/main/java/com/CRM/HKCRM2/config/CustomUsuarioDetalhesService.java
@@ -8,6 +8,31 @@ import org.springframework.security.core.userdetails.*;
 
 import org.springframework.stereotype.Service;
 
-public class CustomUsuarioDetalhesService {
+public class CustomUsuarioDetalhesService implements UserDetailsService {
 
+    @Autowired
+    private UIUsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Busca o usuário pelo email no repositório
+        UsuarioMod usuario = usuarioRepository.findByEmail(email);
+        // Se o usuário não for encontrado, lança uma exceção
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+
+        // Converte as permissões do usuário em uma lista de autoridades
+        var authorities = usuario.getRoles()
+                .stream().map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        // Retorna um objeto UserDetails com as informações do usuário
+        
+        // Devolve um objeto UserDetails com as informações do usuário (email, senha e permissões)
+        return new org.springframework.security.core.userdetails.User(
+                usuario.getEmail(),
+                usuario.getSenha(),
+                authorities
+        );
+    }
 }
