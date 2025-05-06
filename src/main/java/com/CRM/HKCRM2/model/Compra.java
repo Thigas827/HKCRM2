123e8df433a13fac2e2d6034d2d78ab499d27917
Thiago dos Santos Barbosa <thiagocomsilva10@gmail.com>
2025-05-06 16:57:37 -0300

    adiciona a classe Compra com atributos id, cliente e dataCompra

---
+++ src/main/java/com/CRM/HKCRM2/model/Compra.java
@@ -0,0 +1,30 @@
+package com.CRM.HKCRM2.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "compra")
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private UsuarioMod cliente;
+
+
+
+    @Column(name = "data_compra", nullable = false)
+    private LocalDateTime dataCompra = LocalDateTime.now();
+
+}
